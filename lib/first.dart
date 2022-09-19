49a3e244386d8f55bebc7662640f8c85dac56d72
mahroof <mahroofanchukandan@gmail.com>
2022-09-19 16:09:09 +0530

    commit

---
+++ lib/first.dart
@@ -0,0 +1,21 @@
+
+
+void main() {
+
+
+String name="anna";
+int age =20;
+double mark =9.5;
+String email ="abc@gmail.com";
+  int phone =7777000774;
+
+print('my name is $name');
+print(' i am $age years old');
+print('my mark percentage  is $mark');
+print('my email id is $email');
+print('my mobile number is $phone');
+
+
+
+
+}
